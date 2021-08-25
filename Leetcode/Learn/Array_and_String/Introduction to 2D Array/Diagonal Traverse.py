018f87d894fe051de8473a5bea150d3272e3cf94
WOOSHIK <44994859+WOOSHIK-M@users.noreply.github.com>
2021-08-25 23:14:29 +0900

    Make new directory and solved Diagonal Traverse

---
+++ Leetcode/Learn/Array_and_String/Introduction to 2D Array/Diagonal Traverse.py
@@ -0,0 +1,56 @@
+"""
+https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+
+Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+
+-> Example 1:
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+
+->Example 2:
+Input: mat = [[1,2],[3,4]]
+Output: [1,2,3,4]
+
+Constraint:
+1. m == mat.length
+2. n == mat[i].length
+3. 1 <= m, n <= 104
+4. 1 <= m * n <= 104
+5. -105 <= mat[i][j] <= 105
+"""
+from typing import List, Dict
+from collections import defaultdict
+from itertools import chain
+
+
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        # Time: O(m*n)
+        # Space: O(m*n)
+        d = defaultdict(list)
+        for i, r in enumerate(mat):
+            for j, c in enumerate(r):
+                d[i+j] += c,
+
+        return chain.from_iterable(v if k % 2 else v[::-1] for k, v in d.items())
+
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        # Time: O(m*n)
+        # Space: O(m*n)
+        answer: List[int] = []
+        
+        sum_table: Dict[int, List[int]] = defaultdict(list)
+
+        m, n = len(mat), len(mat[0])
+        for row in range(m):
+            for col in range(n):
+                sum_table[row + col].append(mat[row][col])
+
+        for k, v_s in sum_table.items():
+            if k % 2 == 0:
+                [answer.append(v) for v in v_s[::-1]]
+            else:
+                [answer.append(v) for v in v_s]
+
+        return answer
---
+++ Leetcode/Learn/Array_and_String/Introduction to 2D Array/Introduction to 2D Array.cpp
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1166/
+
+Similar to a one-dimensional array, a two-dimensional array also consists of a sequence of elements. But the elements can be laid out in a rectangular grid rather than a line.
+*/
+#include <iostream>
+
+using namespace std;
+
+template <size_t n, size_t m>
+void printArray(int (&a)[n][m]) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cout << "Example I:" << endl;
+    int a[2][5];
+    printArray(a);
+    cout << "Example II:" << endl;
+    int b[2][5] = {{1, 2, 3}};
+    printArray(b);
+    cout << "Example III:"<< endl;
+    int c[][5] = {1, 2, 3, 4, 5, 6, 7};
+    printArray(c);
+    cout << "Example IV:" << endl;
+    int d[][5] = {{1, 2, 3, 4}, {5, 6}, {7}};
+    printArray(d);
+}
