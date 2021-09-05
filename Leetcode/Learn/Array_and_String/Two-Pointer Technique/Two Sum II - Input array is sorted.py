88b83d78587c88a3475b9e22edfefc3aae82fe67
wooshik-m <44994859+WOOSHIK-M@users.noreply.github.com>
2021-09-05 17:06:24 +0900

    Fixed minor issue

--- Leetcode/Learn/Array_and_String/Two-Pointer Technique/Two Sum II - Input array is sorted.py
+++ Leetcode/Learn/Array_and_String/Two-Pointer Technique/Two Sum II - Input array is sorted.py
@@ -44,11 +44,11 @@ class Solution:
     def twoSum(self, numbers, target):
         # Time: O(n)
         # Space: O(1)
-        l, r = 0, len(numbers)-1
+        l, r = 0, len(numbers) - 1
         while l < r:
             s = numbers[l] + numbers[r]
             if s == target:
-                return [l+1, r+1]
+                return [l + 1, r + 1]
             elif s < target:
                 l += 1
             else:
