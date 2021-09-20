63f84cf7a75b6b890ae7d37d662458eccab3d28b
wooshik-m <44994859+WOOSHIK-M@users.noreply.github.com>
2021-09-20 14:53:55 +0900

    Done - Remove Element

--- Leetcode/Learn/Array_and_String/Conclusion/Rotate Array.py
+++ Leetcode/Learn/Array_and_String/Conclusion/Rotate Array.py
@@ -15,7 +15,7 @@ rotate 3 steps to the right: [5,6,7,1,2,3,4]
 -> Example 2:
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
-Explanation: 
+Explanation:
 rotate 1 steps to the right: [99,-1,-100,3]
 rotate 2 steps to the right: [3,99,-1,-100]
 
@@ -35,5 +35,19 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        Time: O(1)
+        Space: O(n)
         """
-        
+        nums = nums[-k:] + nums
+        del nums[-k:]
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Time: O(1)
+        Space: O(n)
+        """
+        k = k % len(nums)
+        if not k:
+            return
+        nums[:k], nums[k:] = nums[-k:], nums[:-k]
