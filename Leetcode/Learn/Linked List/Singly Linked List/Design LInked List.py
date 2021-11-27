3aec7da61cc9d986a3adcb55c647046fa2ca9587
wooshik-m <wooshik.m@gmail.com>
2021-11-27 23:07:45 +0900

    Add question

--- Leetcode/Learn/Linked List/Singly Linked List/Design LInked List.py
+++ Leetcode/Learn/Linked List/Singly Linked List/Design LInked List.py
@@ -32,3 +32,31 @@ Constraints:
 2. Please do not use the built-in LinkedList library.
 3. At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and deleteAtIndex.
 """
+class MyLinkedList:
+
+    def __init__(self):
+        
+
+    def get(self, index: int) -> int:
+        
+
+    def addAtHead(self, val: int) -> None:
+        
+
+    def addAtTail(self, val: int) -> None:
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
