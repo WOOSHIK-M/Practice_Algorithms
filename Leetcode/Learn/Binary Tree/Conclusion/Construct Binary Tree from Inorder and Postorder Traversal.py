b604e48706c5718cd8c43cecab9f796da7b2170d
wooshik-m <wooshik.m@gmail.com>
2021-12-15 11:06:00 +0900

    Make

---
+++ Leetcode/Learn/Binary Tree/Conclusion/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/explore/learn/card/data-structure-tree/133/conclusion/942/
+
+
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+
+-> Example 1:
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+
+-> Example 2:
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+
+Constraints:
+1. 1 <= inorder.length <= 3000
+2. postorder.length == inorder.length
+3. -3000 <= inorder[i], postorder[i] <= 3000
+4. inorder and postorder consist of unique values.
+5. Each value of postorder also appears in inorder.
+6. inorder is guaranteed to be the inorder traversal of the tree.
+7. postorder is guaranteed to be the postorder traversal of the tree.
+"""
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    """TreeNode class."""
+
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ) -> None:
+        """Initialize."""
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        """Build a Tree."""
